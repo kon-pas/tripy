0644a5cf974945c56ad01ebed94df00c3e196248
Konrad <konpas@mail.com>
2022-01-11 01:33:17 +0100

    12

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,10 +1,13 @@
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 export class Card {
   constructor(
-      type="placeholder",
-      name="placeholder",
+      type,
+      name,
       image="https://plikimpi.krakow.pl/pliki/243693/4.jpg",
       desc="Opis",
-      rating=5.0,
+      rating= randomIntFromInterval(0,5),
       link="www.x.com",
       contact="example@gmail.com",
       price=100){
@@ -22,10 +25,6 @@ export class Card {
       key: Math.random()
     };
   }
-
-  // get image() { return this.image };
-  // set image(e) { this.image = e};
-
   getName() { return this.name };
   getImage() { return this.image };
   getDesc() { return this.desc };
@@ -37,4 +36,44 @@ export class Card {
   getType() {return this.meta.type};
   getKey() {return this.meta.key};
 }
+
+// export class Card {
+//   constructor(
+//       type="placeholder",
+//       name="placeholder",
+//       image="https://plikimpi.krakow.pl/pliki/243693/4.jpg",
+//       desc="Opis",
+//       rating=5.0,
+//       link="www.x.com",
+//       contact="example@gmail.com",
+//       price=100){
+
+//     this.name = name;
+//     this.image = image;
+//     this.desc = desc;
+//     this.rating = rating;
+//     this.link = link;
+//     this.contact = contact;
+//     this.price = price;
+
+//     this.meta = {
+//       type: type,
+//       key: Math.random()
+//     };
+//   }
+
+//   // get image() { return this.image };
+//   // set image(e) { this.image = e};
+
+//   getName() { return this.name };
+//   getImage() { return this.image };
+//   getDesc() { return this.desc };
+//   getRating() { return this.rating };
+//   getLink() { return this.link };
+//   getContact() { return this.contact };
+//   getPrice() { return this.price };
+  
+//   getType() {return this.meta.type};
+//   getKey() {return this.meta.key};
+// }
 
--- src/scripts/User.js
+++ src/scripts/User.js
@@ -1,39 +1,73 @@
-export class Card {
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+export class User {
   constructor(
-      type="placeholder",
-      name="placeholder",
-      image="https://plikimpi.krakow.pl/pliki/243693/4.jpg",
-      desc="Opis",
-      rating=5.0,
-      link="www.x.com",
-      contact="example@gmail.com",
-      price=100){
+      id,
+      email,
+      password,
+      name,
+      surname
+  ){
 
-    this.name = name;
-    this.image = image;
-    this.desc = desc;
-    this.rating = rating;
-    this.link = link;
-    this.contact = contact;
-    this.price = price;
+      this.id = id;
+      this.email = email;
+      this.password = password;
+      this.name = name;
+      this.surname = surname;
 
-    this.meta = {
-      type: type,
-      key: Math.random()
-    };
+      this.meta = {
+          type: type,
+          key: Math.random()
+      };
   }
-
-  // get image() { return this.image };
-  // set image(e) { this.image = e};
-
   getName() { return this.name };
-  getImage() { return this.image };
+  getSurname() {return this.surname};
+  getEmail() {return this.email}
+  getId() {return this.id}
   getDesc() { return this.desc };
-  getRating() { return this.rating };
-  getLink() { return this.link };
   getContact() { return this.contact };
-  getPrice() { return this.price };
-  
+
   getType() {return this.meta.type};
   getKey() {return this.meta.key};
 }
+
+// export class Card {
+//   constructor(
+//       type="placeholder",
+//       name="placeholder",
+//       image="https://plikimpi.krakow.pl/pliki/243693/4.jpg",
+//       desc="Opis",
+//       rating=5.0,
+//       link="www.x.com",
+//       contact="example@gmail.com",
+//       price=100){
+
+//     this.name = name;
+//     this.image = image;
+//     this.desc = desc;
+//     this.rating = rating;
+//     this.link = link;
+//     this.contact = contact;
+//     this.price = price;
+
+//     this.meta = {
+//       type: type,
+//       key: Math.random()
+//     };
+//   }
+
+//   // get image() { return this.image };
+//   // set image(e) { this.image = e};
+
+//   getName() { return this.name };
+//   getImage() { return this.image };
+//   getDesc() { return this.desc };
+//   getRating() { return this.rating };
+//   getLink() { return this.link };
+//   getContact() { return this.contact };
+//   getPrice() { return this.price };
+  
+//   getType() {return this.meta.type};
+//   getKey() {return this.meta.key};
+// }
