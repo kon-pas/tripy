134c86c0a36498c1fb28d313bb7f11037c44396d
kon-pas <konpas@mail.com>
2022-01-22 20:33:09 +0100

    20

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -113,42 +113,42 @@ class InputForm extends Component {
                 value={this.state.wylot}
                 onChange={this.handleChange}
                 name="wylot"
-                placeholder="Wylot (W.I.P.)"
+                placeholder="Wylot"
               />
               <input className="powrot"
                 type="text"
                 value={this.state.powrot}
                 onChange={this.handleChange}
                 name="powrot"
-                placeholder="Powrót (W.I.P.)"
+                placeholder="Powrót"
               />
               <input className="miejscowosc"
                 type="text"
                 value={this.state.miejscowosc}
                 onChange={this.handleChange}
                 name="miejscowosc"
-                placeholder="Miejscowość (W.I.P.)"
+                placeholder="Miejscowość"
               />
               <input className="liczba-osob"
                 type="text"
                 value={this.state.liczba_osob}
                 onChange={this.handleChange}
                 name="liczbaOsob"
-                placeholder="Liczba osób (W.I.P.)"
+                placeholder="Liczba osób"
               />
-              <input className="budzet-minimalny (W.I.P.)"
+              <input className="budzet-minimalny"
                 type="text"
                 value={this.state.budzet_minimalny}
                 onChange={this.handleChange}
                 name="budzetMinimalny"
-                placeholder="Budżet minimalny (W.I.P.)"
+                placeholder="Budżet minimalny"
               />
               <input className="budzet-maksymalny"
                 type="text"
                 value={this.state.budzet_maksymalny}
                 onChange={this.handleChange}
                 name="budzetMaksymalny"
-                placeholder="Budżet maksymalny (W.I.P.)"
+                placeholder="Budżet maksymalny"
               />
             </div>
             <input className="submit-button"
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -9,11 +9,13 @@ export const searchSettings = {
   setWylot: function(arg) {this.wylot = arg},
   setPowrot: function(arg) {this.email = arg},
   setMiejscowosc: function(arg) {this.miejscowosc = arg},
+  setLiczbaOsob: function(arg) {this.liczbaOsob = arg},
   setBudzetMinimalny: function(arg) {this.budzetMinimalny = arg},
   setBudzetMaksymalny: function(arg) {this.budzetMaksymalny = arg},
 
   getWylot: function() { return this.wylot },
   getPowrot: function() { return this.email },
+  getMiejscowosc: function() { return this.miejscowosc },
   getLiczbaOsob: function() { return this.liczbaOsob },
   getBudzetMinimalny: function() { return this.budzetMinimalny },
   getBudzetMaksymalny: function() { return this.budzetMaksymalny }
